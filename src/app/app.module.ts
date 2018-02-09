1a5c7d43509e907c22e5b1fa448ff87cf1442f98
LuisBragado <luis.bragado.mx@gmail.com>
2018-02-09 04:14:21 -0600

    Creación de un nuevo componente: Empleado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {FrutaComponent} from './fruta/fruta.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FrutaComponent
+    FrutaComponent,
+    EmpleadoComponent
   ],
   imports: [
     BrowserModule
